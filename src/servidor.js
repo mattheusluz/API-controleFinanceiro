0aabacb1d49594ec510afd026d6734bfe0b89060
mattheusluz <80133489+mattheusluz@users.noreply.github.com>
2022-04-02 15:49:11 -0300

    Configurado arquivos base do projeto

---
+++ src/servidor.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const rotas = require('./rotas');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors);
+app.use(rotas);
+
+module.exports = app;
