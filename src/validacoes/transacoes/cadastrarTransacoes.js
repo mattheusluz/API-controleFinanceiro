b9e0d13c3394c365892e42b5f50b23b308524525
mattheusluz <80133489+mattheusluz@users.noreply.github.com>
2022-04-17 15:11:32 -0300

    Ajuste validação de data

--- src/validacoes/transacoes/cadastrarTransacoes.js
+++ src/validacoes/transacoes/cadastrarTransacoes.js
@@ -4,7 +4,6 @@ const schemaCadastrarTransacao = yup.object().shape({
   tipo: yup.boolean().required(),
   dia_semana: yup.string().required(),
   descricao: yup.string().required(),
-  data: yup.required(),
   categoria: yup.string().required(),
   valor: yup.number().required(),
 });
